fedfebfdc6c2422743e9609380d4e132f04719a4
Dean Slama <deanslamajr@gmail.com>
2015-06-13 16:54:38 -0500

    Added mongojs talking to mongolab, need to implement grabbing data

--- routes.js
+++ routes.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var mongojs = require('mongojs');
 
 var router = express.Router();
+//var dbPath = process.env.MONGOLAB_URI || 27017
+
+//local db:
+//var db = mongojs('localPic4Ward', ['localPic4Ward']);
+var db = mongojs( 'heroku_70wln8p7:ckrcrt2u2s98sq78job27md3pk@ds043952.mongolab.com:43952/heroku_70wln8p7', ['PicObjects']);
+
+db.PicObjects.find(function(err, docs) {
+    console.log(docs); 
+    console.log(err);
+});
 
 router.get('/', function (req, res){
+	
+
 	res.render('index', {
 		title: 'My app'
 	});
