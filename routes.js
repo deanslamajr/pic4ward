b8c00462b90dbe1aad3ad83112eba8229ce8d4fd
Dean Slama <deanslamajr@gmail.com>
2015-06-13 18:43:16 -0500

    GET / now pulls pic url from MongoLab

--- routes.js
+++ routes.js
@@ -2,22 +2,24 @@ var express = require('express');
 var mongojs = require('mongojs');
 
 var router = express.Router();
-//var dbPath = process.env.MONGOLAB_URI || 27017
 
 //local db:
 //var db = mongojs('localPic4Ward', ['localPic4Ward']);
 var db = mongojs( 'heroku_70wln8p7:ckrcrt2u2s98sq78job27md3pk@ds043952.mongolab.com:43952/heroku_70wln8p7', ['PicObjects']);
 
-db.PicObjects.find(function(err, docs) {
-    console.log(docs); 
-    console.log(err);
-});
+
 
 router.get('/', function (req, res){
-	
+	var picId = '557c7ccfe4b00a69307df6d9';
 
-	res.render('index', {
-		title: 'My app'
+	db.PicObjects.findOne({_id:mongojs.ObjectId(picId)},function(err, docs) {
+		if (!err) {
+			var picObj = docs; 
+			res.render('index', {'picObj': picObj} );
+		} else {
+			console.log(err);
+			res.send('Whoops!!');
+		}
 	});
 });
 
