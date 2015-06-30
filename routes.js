659f75d5373ba20ce33630e6eb388d5c1f4df209
Dean Slama <deanslamajr@gmail.com>
2015-06-30 18:39:17 -0500

    Fixed some AWS S3 linking issues

--- routes.js
+++ routes.js
@@ -5,39 +5,36 @@ var router = express.Router();
 
 //local db:
 //var db = mongojs('localPic4Ward', ['localPic4Ward']);
-var db = mongojs( 'heroku_70wln8p7:ckrcrt2u2s98sq78job27md3pk@ds043952.mongolab.com:43952/heroku_70wln8p7', ['PicObjects']);
-
-
+var db = mongojs( 'heroku_70wln8p7:ckrcrt2u2s98sq78job27md3pk@ds043952.mongolab.com:43952/heroku_70wln8p7', ['PicObjects']); 
 
 router.get('/', function (req, res){
-	var picId = '557c7ccfe4b00a69307df6d9';
-
-	db.PicObjects.findOne({_id:mongojs.ObjectId(picId)},function(err, doc) {
-		if (!err) {
-			var picObj = doc;
-			var clickables = doc.clickables;
-			res.render('index', {'picObj': picObj, 'clickables': clickables, 'newClickables': JSON.stringify(clickables)} );
-		} else {
-			console.log(err);
-			res.send('Whoops!!');
-		}
-	});
+  var picId = '557c7ccfe4b00a69307df6d9';
+  db.PicObjects.findOne({_id:mongojs.ObjectId(picId)}, function(err, doc) {
+    if (doc) {
+      var name    = doc.name;
+      var clickables  = doc.clickables;
+      var url     = doc.url;
+      res.render('index', {'name': name, 'url': url, 'newClickables': JSON.stringify(clickables)} );
+    } else {
+      console.log(err);
+      res.send('Whoops!!');
+    }
+  });
 });
 
 router.get('/:id', function (req, res){
-	var reqGrabber = req.params.id;
-	console.log(reqGrabber);
-
-	db.PicObjects.findOne({grabber: reqGrabber },function(err, doc) {
-		if (doc) {
-			var picObj = doc;
-			var clickables = doc.clickables;
-			res.render('index', {'picObj': picObj, 'clickables': clickables, 'newClickables': JSON.stringify(clickables)} );
-		} else {
-			console.log(err);
-			res.send('Whoops!!');
-		}
-	});
+  var reqGrabber = req.params.id;
+  db.PicObjects.findOne({grabber: reqGrabber }, function(err, doc) {
+    if (doc) {
+      var name    = doc.name;
+      var clickables  = doc.clickables;
+      var url     = doc.url;
+      res.render('index', {'name': name, 'url': url, 'newClickables': JSON.stringify(clickables)} );
+    } else {
+      console.log(err);
+      res.send('Whoops!!');
+    }
+  });
 });
 
 module.exports = router;
